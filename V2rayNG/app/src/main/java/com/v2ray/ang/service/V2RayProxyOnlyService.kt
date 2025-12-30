8e10881846a03d0c5e2995cdbceb026d110f1581
MjKhani <130058196+MjKhani@users.noreply.github.com>
2025-12-30 13:15:35 +0330

    Update V2RayProxyOnlyService.kt

--- V2rayNG/app/src/main/java/com/v2ray/ang/service/V2RayProxyOnlyService.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/service/V2RayProxyOnlyService.kt
@@ -13,7 +13,6 @@ import com.v2ray.ang.util.MyContextWrapper
 import java.lang.ref.SoftReference
 
 class V2RayProxyOnlyService : Service(), ServiceControl {
-
     override fun onCreate() {
         super.onCreate()
         V2RayServiceManager.serviceControl = SoftReference(this)
@@ -25,25 +24,20 @@ class V2RayProxyOnlyService : Service(), ServiceControl {
     }
 
     override fun onDestroy() {
-        // اطمینان از بسته شدن کامل در زمان نابودی سرویس
         V2RayServiceManager.stopCoreLoop()
         NotificationManager.cancelNotification()
         super.onDestroy()
     }
 
     override fun getService(): Service = this
-    override fun startService() { }
-    override fun stopService() {
-        stopSelf()
-    }
+    override fun startService() {}
+    override fun stopService() = stopSelf()
     override fun vpnProtect(socket: Int): Boolean = true
     override fun onBind(intent: Intent?): IBinder? = null
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun attachBaseContext(newBase: Context?) {
-        val context = newBase?.let {
-            MyContextWrapper.wrap(newBase, SettingsManager.getLocale())
-        }
+        val context = newBase?.let { MyContextWrapper.wrap(it, SettingsManager.getLocale()) }
         super.attachBaseContext(context)
     }
 }
