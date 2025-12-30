378d9588871bbebe5d1617d0341b60052710b41f
MjKhani <130058196+MjKhani@users.noreply.github.com>
2025-12-30 13:14:50 +0330

    Update SubscriptionUpdater.kt

--- V2rayNG/app/src/main/java/com/v2ray/ang/handler/SubscriptionUpdater.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/handler/SubscriptionUpdater.kt
@@ -5,7 +5,6 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
-import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.work.CoroutineWorker
@@ -14,35 +13,28 @@ import com.v2ray.ang.AppConfig
 import com.v2ray.ang.R
 
 object SubscriptionUpdater {
-    class UpdateTask(context: Context, params: WorkerParameters) :
-        CoroutineWorker(context, params) {
-
-        private val notificationManager = NotificationManagerCompat.from(applicationContext)
-
+    class UpdateTask(context: Context, params: WorkerParameters) : CoroutineWorker(context, params) {
         @SuppressLint("MissingPermission")
         override suspend fun doWork(): Result {
             val subs = MmkvManager.decodeSubscriptions().filter { it.second.autoUpdate }
             if (subs.isEmpty()) return Result.success()
 
-            val channelId = "subscription_update_channel"
+            val nm = NotificationManagerCompat.from(applicationContext)
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                val channel = NotificationChannel(channelId, "Subscription Update", NotificationManager.IMPORTANCE_MIN)
-                notificationManager.createNotificationChannel(channel)
+                nm.createNotificationChannel(NotificationChannel(AppConfig.SUBSCRIPTION_UPDATE_CHANNEL, AppConfig.SUBSCRIPTION_UPDATE_CHANNEL_NAME, NotificationManager.IMPORTANCE_MIN))
             }
 
-            val notification = NotificationCompat.Builder(applicationContext, channelId)
+            val builder = NotificationCompat.Builder(applicationContext, AppConfig.SUBSCRIPTION_UPDATE_CHANNEL)
                 .setSmallIcon(R.drawable.ic_stat_name)
                 .setContentTitle(applicationContext.getString(R.string.title_pref_auto_update_subscription))
                 .setPriority(NotificationCompat.PRIORITY_MIN)
-                .setOngoing(false)
 
             for (sub in subs) {
-                notification.setContentText("Updating ${sub.second.remarks}")
-                notificationManager.notify(3, notification.build())
+                builder.setContentText("Updating ${sub.second.remarks}")
+                nm.notify(3, builder.build())
                 AngConfigManager.updateConfigViaSub(Pair(sub.first, sub.second))
             }
-
-            notificationManager.cancel(3)
+            nm.cancel(3)
             return Result.success()
         }
     }
